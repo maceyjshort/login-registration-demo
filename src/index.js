ea5121c1261ac0f35d0eec2cfa0aa8621df53f22
Macey Short <macey.walker1991@gmail.com>
2022-10-12 18:32:52 -0500

    Addd commits

--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ import Header from "./components/Header";
 
 const root = document.getElementById("root");
 
+// there is usually app compenont that serves of top level compenoent
 const App = () => {
+  // State represents all moving parts of applications
+  // State is an object that contains all the data of the application needs
+  // State will be updated based on user input and/or data from the server
   const state = {
     title: "My Form",
     fields: ["email", "password"],
@@ -13,14 +17,19 @@ const App = () => {
     },
   };
 
+  // whenever we can pass state from a parent componenet to a child compent
+  // state can be  passed as props
   return `
     ${Header(state)}
     ${Form(state)}
   `;
 };
 
+// render the app compenent to the DOM
 function render() {
   root.innerHTML = App();
 }
 
 render();
+// compents are basically standalone function elements that we can pass into
+// react is a library
