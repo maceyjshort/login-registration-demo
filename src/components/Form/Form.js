cc7a855d094a5c9c624272f1da994ac6a20bc521
Macey Short <macey.walker1991@gmail.com>
2022-10-12 18:43:31 -0500

    Add commit notes

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,9 +1,15 @@
 import Button from "./Button";
 import Input from "./Input";
 
+// map over array of strings and return input component of each one
+// map is a must have for react- do more practice with map
 export default ({ fields, button }) => `
   <form class="mx-auto mt-2 max-w-xs">
-    ${fields.map((input) => Input({ label: input })).join("")}
-    ${Button({ ...button })}
+    ${fields.map((field) => Input({ label: field })).join("")}
+    ${Button(button)}
   </form>
 `;
+
+// form has several childern-button and input
+// the fields and button is getting passed into it
+// app passes prop to form that passes from of the info to form
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,2 +1,6 @@
 export default ({ title }) =>
   `<h1 class="my-4 text-center text-3xl font-semibold">${title}</h1>`;
+
+// Header remains us that it is a compoent
+// title knows it will recieve something to be passed on something
+// title is a prop being passed to a child
